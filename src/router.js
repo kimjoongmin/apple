ddae73b2e6b777bb225ea9b011403fc679e14aec
kimjoongmin <82187845+kimjoongmin@users.noreply.github.com>
2021-04-16 19:01:41 +0900

    0416

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
 const routes = [
-    {path: '/apple', name: 'Home', component: () => import('@/views/Home')},
-    {path: '/apple/About', name: 'About', component: () => import('@/views/About')},
-    {path: '/apple/Rooms', name: 'Rooms', component: () => import('@/views/Rooms')},
-    {path: '/apple/Game', name: 'Game', component: () => import('@/views/Game')},
+    {path: '/', name: 'Home', component: () => import('@/views/Home')},
+    {path: '/About', name: 'About', component: () => import('@/views/About')},
+    {path: '/Rooms', name: 'Rooms', component: () => import('@/views/Rooms')},
+    {path: '/Game', name: 'Game', component: () => import('@/views/Game')},
   ];
 
 export const router = createRouter({
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath:'/apple'
+    publicPath:'/'
 }
