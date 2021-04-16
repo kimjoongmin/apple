a9b9b1d33a8f44158710f39f2c70ea6af6e91d88
kimjoongmin <82187845+kimjoongmin@users.noreply.github.com>
2021-04-16 18:04:03 +0900

    0416

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
 const routes = [
-    {path: '/', name: 'Home', component: () => import('@/views/Home')},
+    {path: '/apple', name: 'Home', component: () => import('@/views/Home')},
     {path: '/About', name: 'About', component: () => import('@/views/About')},
     {path: '/Rooms', name: 'Rooms', component: () => import('@/views/Rooms')},
     {path: '/Game', name: 'Game', component: () => import('@/views/Game')},
